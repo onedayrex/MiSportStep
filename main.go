790a7e6bca18bd6f63236c67f05f328a584e929d
zhongfuxiang <1115757866@qq.com>
2021-07-02 17:30:49 +0800

    添加环境变量

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
-import "sport"
+import (
+	"os"
+	"sport"
+)
 
 func main() {
+	userName := os.Getenv("XIAOMI_USERNAME")
+	password := os.Getenv("XIAOMI_PASSWORD")
+	stepRang := os.Getenv("XIAOMI_STEP_RANG")
 	s := src.Sport{
-		UserName: "",
-		Password: "",
-		StepRang: "8888-9999",
+		UserName: userName,
+		Password: password,
+		StepRang: stepRang,
 	}
 	s.Login()
 	s.PushSetp()
